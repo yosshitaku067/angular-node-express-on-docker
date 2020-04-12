2d540dfa76935f9ca977cf4d1b9ca231e5bd2f0e
yosshitaku067 <y.takuya77@gmail.com>
2020-04-12 15:37:48 +0900

    ini

---
+++ app/express-app/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.redirect('/angular-app/');
+});
+
+app.use(express.static('www'));
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
